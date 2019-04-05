548da302bd6b16d9498b83f41cd8ddf4e287eef4
bluerat1976 <osoblivaia76@mail.ru>
2019-04-05 21:04:45 +0300

    Right example

---
+++ index-2.js
@@ -0,0 +1,56 @@
+/*
+var arr = [0, 5, 8, 10, 100, -1];
+
+console.warn(arr);
+
+console.error(arr);
+
+ {
+    console.log('arr with index ', i, ' = ', arr[i]);
+}
+
+for(var i = arr.length-1; i >= 0; i--) {
+    console.log('arr with index ', i, ' = ', arr[i]);
+}
+
+for(var i = 0; i < arr.length; i = i + 2) {
+    console.log('arr with index ', i, ' = ', arr[i]);
+}
+console.log('other cycles');
+forEach,
+for( in ),
+
+console.log('while')
+i = 0;
+while (i < arr.length) {
+    console.log('arr with index ', i, ' = ', arr[i]);
+    i++;
+}
+
+console.log('do while')
+i = 0;
+do{
+    console.log('arr with index ', i, ' = ', arr[i]);
+    i++;
+}while (i < arr.length);
+
+
+console.log('for (in)');
+
+var car = {
+    model: 'toyota',
+    volum: '1.5',
+    power: 100
+}
+console.log('our object', car);
+console.log('our object', Object.keys(car));
+for(var key in car) {
+    console.log('car has', key, 'with value =', car[key] )
+}
+console.log('cycle for object with simple sycle for');
+var keys = Object.keys(car);
+for(let i = 0; i < keys.length; i++) {
+    var currentKey = keys[i];
+    console.log('car has kay', currentKey, 'value')
+}
+*/
